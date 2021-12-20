1e87105059c7646eddaf03f64733e57005d9f527
Carlos Oliveira <carloshenrique.o.e@gmail.com>
2021-12-20 20:02:00 -0300

    more changes

--- flutter_swift_teste/lib/sides/left_side_widget.dart
+++ flutter_swift_teste/lib/sides/left_side_widget.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swift_teste/components/leftside/minus_button_widget.dart';
+
+import '../components/leftside/arrow_widget.dart';
+import '../components/leftside/minus_button_widget.dart';
+import '../components/others/button_widget.dart';
 
 class LeftSideWidget extends StatelessWidget {
-  const LeftSideWidget({Key? key}) : super(key: key);
+  final double width;
+  final double height;
+
+  const LeftSideWidget({
+    Key? key,
+    this.width = 95,
+    this.height = 231,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +20,35 @@ class LeftSideWidget extends StatelessWidget {
 
     //CRIAR UM PADDING RESPONSÍVEL.
     //CALUCULAR O VALOR DA LARGRURA DO ESPAÇAMENTO DIVIDIDO PELA LARGURA DA LADO = 12 / 375
-    return Column(
-      children: [
-        Align(
-          child: MinusButtonWidget(
-            width: size.width * 0.19,
-            heigth: size.height * 0.021,
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              LayoutBuilder(builder: (context, constraints) {
+                return Align(
+                  child: MinusButtonWidget(
+                    width: size.width * 0.05,
+                  ),
+                  alignment: Alignment.topLeft,
+                );
+              }),
+              LayoutBuilder(builder: (context, constraints) {
+                return Align(
+                  child: ButtonWidget(
+                    size: constraints.maxWidth * 0.29,
+                    button: Button.up,
+                    child: const ArrowWidget(Direction.up),
+                  ),
+                  alignment: Alignment.bottomCenter,
+                );
+              }),
+            ],
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
